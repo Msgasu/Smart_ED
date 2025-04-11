a1dd850a84bddd25150c9823141d7aa494356941
Msgasu <edamg123@gmail.com>
2025-04-11 14:57:52 +0000

    Refactor AdminLayout to include header with notifications icon

--- src/components/admin/AdminLayout.jsx
+++ src/components/admin/AdminLayout.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import Sidebar from './Sidebar';
 import './styles/AdminLayout.css';
+import NotificationsIcon from '../common/NotificationsIcon';
 
 const AdminLayout = ({ children }) => {
   return (
     <div className="admin-layout">
       <Sidebar />
-      <main className="main-content">
-        {children}
-      </main>
+      <div className="content-wrapper">
+        <header className="admin-header">
+          <div className="header-title">
+            {/* Page title could be dynamic if needed */}
+          </div>
+          <div className="header-actions">
+            <NotificationsIcon />
+            {/* Other header actions can be added here */}
+          </div>
+        </header>
+        <main className="main-content">
+          {children}
+        </main>
+      </div>
     </div>
   );
 };
