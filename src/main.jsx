47755103e4dbf67c39bd99a4bc3a679541a10c2b
Msgasu <edamg123@gmail.com>
2025-04-26 15:50:24 +0000

    Wrap App component with FontSizeProvider to manage font size context across the application.

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,12 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 import './index.css'
+import { FontSizeProvider } from './context/FontSizeContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <FontSizeProvider>
+      <App />
+    </FontSizeProvider>
   </React.StrictMode>
 )
